bfaa01f9c4c6226034b1d0c26904d497507e4f8b
Fabrício Ferrão Araujo <araujoffabricio@gmail.com>
2025-02-23 23:31:45 -0300

    aulas 1 e 2, pós PUCRS, DEVSoltinho

---
+++ Aula 2/map.js
@@ -0,0 +1,18 @@
+//TDD - TEsst dRiven Development
+
+var novoMapa = new Map();
+
+class HistoryMAP {}
+
+//11/11/2011 às 11 da noite
+novoMapa.set('11/11/11/2011 - 23:30 - URL', { title: 'Titulo do site', url: 'sozinha'});
+novoMapa.set('11/11/11/2011 - 23:30 - URL', {});
+novoMapa.set('11/11/11/2011 - 23:30 - URL', {});
+novoMapa.set('11/11/11/2011 - 23:30 - URL', {});
+novoMapa.set('11/11/11/2011 - 23:30 - URL', {});
+novoMapa.set('11/11/11/2011 - 23:30 - URL', {});
+novoMapa.set('11/11/11/2011 - 23:30 - URL', {});
+novoMapa.set('11/11/11/2011 - 23:31 - URL', {});
+
+var resultado = novoMapa;
+console.log(resultado)
